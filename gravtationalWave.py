2d8464ef91563d69292d58df2a6d1db0a52d282e
Sipho Nkele <sipsnkele@gmail.com>
2023-12-07 08:29:04 +0200

    Optimzed the python code remove redundency and improved readability. Added a main method.

--- gravtationalWave.py
+++ gravtationalWave.py
@@ -1,95 +1,85 @@
 from vpython import*
 import numpy as np
-from math import sin, cos
+from math import sin, cos, pi
 
 
-#____________________________________________________
-# constants
-y0 = 1.0
-x0 = 1.0
-Polarization = "+"
-k  = 2.0
-M = 1.0  # mass of the test particles in the xy plane
-Radius = 0.1; z0 = 2; # Radius and the Z co-ordinates
-Cords = [1 ,  -1 , 0, 1/np.sqrt(2), -1/np.sqrt(2)]
-points = [(0, 2), (1, 2), (2, 0), (2, 1), (3, 3), (3, 4), (4, 4), (4, 3)]
 
-def create_spheres():
+def create_spheres(Cords, points, Radius, z0):
     return [sphere(radius=Radius, color=color.orange, pos=vector(Cords[point[0]], Cords[point[1]], Cords[z0])) for point in points]
 #______________________________________________________
 # h_{+} Polarization
-def Model_Plus():
+def Model_Plus(t, t_max, dt, C, x0, y0, k, Param):
     #____________________________________________________
     # test mass [particles] that
     #will be affected by gravitational wave
     #____________________________________________________
     # motion of the test mass as the wave pass by
-    test_masses = create_spheres()
+    test_masses = create_spheres(Param[0], Param[1], Param[2], Param[3])
     xyLine = test_masses[:4] 
     xyQuad = test_masses[4:]
-    dt = 0.01
-    t = 0
-    while (t < 100):
-        rate(100)
-        dx = 1-(1/2)*x0*sin(k*t)
-        dy = 1+(1/2)*y0*sin(k*t)
-        dx1 = 1/np.sqrt(2)-(1/2)*x0*sin(k*t)
-        dy1 = 1/np.sqrt(2)+(1/2)*y0*sin(k*t)
-        xyLine[0].pos.x = dx
-        xyLine[2].pos.y = dy
-        xyLine[3].pos.y =-dy                                                           
-        xyQuad[0].pos.x = dx1
-        xyQuad[0].pos.y =dy1
-        xyQuad[1].pos.x = -dx1
-        xyQuad[1].pos.y = dy1
-        xyLine[1].pos.x =-dx
-        xyQuad[2].pos.x =-dx1
-        xyQuad[2].pos.y =-dy1
-        xyQuad[3].pos.x = dx1
-        xyQuad[3].pos.y = -dy1
-
+    while (t < t_max):
+        rate(t_max)
+        wave = 0.5 * sin(k * t)
+        for i in range(4):
+            if i < 2:
+                xyLine[i].pos.x = 1 - x0 * wave if i == 0 else -(1 - x0 * wave)
+            else:
+                xyLine[i].pos.y = 1 + wave if i == 2 else -(1 + wave)
+        for i in range(4):
+            xyQuad[i].pos.x = C - y0 * wave if (i == 0 or i == 3) else -(C - y0 * wave)
+            xyQuad[i].pos.y = C + y0 * wave if (i == 0 or i == 1) else -(C + y0 * wave) 
         t+=dt
 
 #_________________________________________________________
         # h_{x} Polarization
-def Model_Cross():
+def Model_Cross(t, t_max, dt, C, x0, y0, k, Param):
     #____________________________________________________
     # test mass [particles] that
     #will be affected by gravitational wave
-    test_masses = create_spheres()
+    test_masses = create_spheres(Param[0], Param[1], Param[2], Param[3])
     #____________________________________________________
     # motion of the test mass as the wave pass by
     xyLine = test_masses[:4] 
     xyQuad = test_masses[4:]
-    dt = 0.01
-    t = 0
-    while (t < 100):
-        rate(100)
-        dx =(1/2)*x0*sin((1/4)*np.pi*k*t);
-        dy =(1/2)*y0*sin((1/4)*np.pi*k*t)                                 # the wave is passing to xy-plane and the z-direction
-        dx1 = 1/np.sqrt(2)-(1/2)*x0*sin((1/4)*np.pi*k*t);
-        dy1 = 1/np.sqrt(2)-(1/2)*y0*sin((1/4)*np.pi*k*t)   
-        dx2 = 1/np.sqrt(2)+(1/2)*x0*sin((1/4)*np.pi*k*t);
-        dy2 = 1/np.sqrt(2)+(1/2)*y0*sin((1/4)*np.pi*k*t)
-        xyLine[0].pos.x = dx; xyLine[0].pos.y = 1
-        xyQuad[0].pos.x = -dx1;
-        xyQuad[0].pos.y =dy1
-        xyQuad[1].pos.x = dx2;
-        xyQuad[1].pos.y = dy2
-        xyLine[1].pos.x = -dx;
-        xyLine[1].pos.y = -1
-        xyQuad[2].pos.x =-dx2;
-        xyQuad[2].pos.y =-dy2
-        xyQuad[3].pos.x = dx1;
-        xyQuad[3].pos.y = -dy1
-        xyLine[2].pos.y = -dy;
-        xyLine[2].pos.x = -1
-        xyLine[3].pos.y = dy; xyLine[3].pos.x =1
+    while (t < t_max):
+        rate(t_max)
+        wave = 0.5 * sin(0.25 * pi * k * t)
+        for i in range(4):
+            if i < 2:
+                xyLine[i].pos.x = x0 * wave if i == 0 else -x0 * wave
+                xyQuad[i].pos.x =-(C-x0 * wave) if i == 0 else C + x0 * wave
+                xyLine[i].pos.y = 1 if i == 0 else -1
+                xyQuad[i].pos.y = C-y0 * wave if i == 0 else C + y0 * wave
+            else:
+                xyLine[i].pos.x = -1 if i == 2 else 1
+                xyQuad[i].pos.x =-(C + x0 * wave) if i == 2 else C - x0 * wave
+                xyLine[i].pos.y =-y0 * wave if i == 2 else y0 * wave
+                xyQuad[i].pos.y =-(C + y0 * wave) if i == 2 else -(C - y0 * wave)   
         t+=dt
 #_____________________________________________________________
         # function calls for the Polarizations
-choice = input("Enter your Polarization choice [h_{+} or h_{x}] as + or x: \n")
-if (choice==Polarization):
-    print(Model_Plus())
-else:
-    print(Model_Cross())
+
+
+def main():
+    #____________________________________________________
+    # constants
+    t = 0
+    y0 = 1.0
+    x0 = 1.0
+    k  = 2.0
+    dt = 0.01
+    t_max = 100
+    Radius = 0.1; 
+    z0 = 2; # Radius and the Z co-ordinates
+    A = 1/np.sqrt(2)
+
+    Cords = [1 ,  -1 , 0, 1/np.sqrt(2), -1/np.sqrt(2)]
+    points = [(0, 2), (1, 2), (2, 0), (2, 1), (3, 3), (3, 4), (4, 4), (4, 3)]
+
+    choice = input("Enter your Polarization choice [h_{+} or h_{x}] as + or x: \n")
+    if (choice=="+"):
+        print(Model_Plus(t, t_max, dt, A, x0, y0 ,k, (Cords, points, Radius, z0)))
+    else:
+        print(Model_Cross(t, t_max, dt, A, x0, y0, k, (Cords, points, Radius, z0)))
+if __name__=='__main__':
+    main()
